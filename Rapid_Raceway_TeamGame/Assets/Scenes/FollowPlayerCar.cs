2fdeb2cee7051bf97bbc31d1f4d91cf9949af9ee
Lchesbr <lchesbr@gmail.com>
2025-11-03 18:23:24 -0500

    Added Input for Player Car Movement

---
+++ Rapid_Raceway_TeamGame/Assets/Scenes/FollowPlayerCar.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class FollowPlayerCar : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Offest the camera behind the player by adding to the player's position
+        transform.position = player.transform.position + offset;
+    }
+}
+
